3ac30cac1b4eca37008d39d410790c2d7d6ef448
matiasherranz <matiasherranz@gmail.com>
2022-04-12 16:12:00 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-re4awi
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-re4awi)
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// We are going to want to create a table that renders a list of users that looks like https://imgur.com/a/UkGn28s
+// Fetch data (20 users) from https://randomuser.me/ (follow link for documentation)
+
+// For Guidance
+// 1. You can first create a table with the user data
+// 2. Then you can add sorting
+// 3. Style the table (you can use plain css or tailwind which is included in this sandbox)
+
+// Bonus
+// - add pagination
+// - make the rows expandable and show the user's address when expanded (https://imgur.com/a/L53aHDb)
+
+function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+}
+
+export default App;
